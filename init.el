ddc760534e101c45e4075623e6e71a0665567600
lance bradley <l@ncebradley.org>
2010-10-29 14:59:05 -0700

    initial commit

---
+++ init.el
@@ -0,0 +1,86 @@
+;; turn off emacs startup message
+(setq inhibit-startup-message t)
+
+;; add all subdirs of ~/.emcas.d to your load-path
+(dolist (f (file-expand-wildcards "~/.emacs.d/*"))
+  (add-to-list 'load-path f))
+
+;; load color-theme
+(require 'color-theme)
+(color-theme-initialize)
+(setq color-theme-is-global t)
+;; use wombat
+(load-file "~/.emacs.d/color-theme/themes/wombat.el")
+(color-theme-wombat)
+
+;; default frame size
+(add-to-list 'default-frame-alist (cons 'height 24))
+(add-to-list 'default-frame-alist (cons 'width 80))
+(add-to-list 'default-frame-alist '(alpha 85 75))
+
+;; f5
+(global-set-key [f5] 'revert-buffer)
+
+;; load clojure mode
+(require 'clojure-mode)
+
+;; load slime
+(eval-after-load "slime" 
+  '(progn (slime-setup '(slime-repl))))
+(require 'slime)
+(slime-setup)
+
+;; load clojure test mode 
+(autoload 'clojure-test-mode "clojure-test-mode" "Clojure test mode" t)
+(autoload 'clojure-test-maybe-enable "clojure-test-mode" "" t)
+(add-hook 'clojure-mode-hook 'clojure-test-maybe-enable)
+
+;; load paredit
+(require 'paredit)
+(add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode -1)))
+
+;; correctly tab defprotocols, etc
+(custom-set-variables
+ '(clojure-mode-use-backtracking-indent t))
+
+;; rainbow parentheses
+(require 'highlight-parentheses)
+(add-hook 'clojure-mode-hook '(lambda () (highlight-parentheses-mode 1)))
+(setq hl-paren-colors
+      '("orange1" "yellow1" "greenyellow" "green1"
+	"springgreen1" "cyan1" "slateblue1" "magenta1" "purple"))
+
+;; slime
+;(require 'slime)
+;(require 'slime-repl)
+;(add-hook 'slime-load-hook 'slime-repl-init)                          
+
+;; magic, haven't broken this down yet
+(defmacro defclojureface (name color desc &optional others)
+  `(defface ,name '((((class color)) (:foreground ,color ,@others))) ,desc :group 'faces))
+
+; Dim parens - http://briancarper.net/blog/emacs-clojure-colors
+(defclojureface clojure-parens       "DimGrey"   "Clojure parens")
+(defclojureface clojure-braces       "#49b2c7"   "Clojure braces")
+(defclojureface clojure-brackets     "SteelBlue" "Clojure brackets")
+(defclojureface clojure-keyword      "khaki"     "Clojure keywords")
+(defclojureface clojure-namespace    "#c476f1"   "Clojure namespace")
+(defclojureface clojure-java-call    "#4bcf68"   "Clojure Java calls")
+(defclojureface clojure-special      "#b8bb00"   "Clojure special")
+(defclojureface clojure-double-quote "#b8bb00"   "Clojure special" (:background "unspecified"))
+
+(defun tweak-clojure-syntax ()
+  (mapcar (lambda (x) (font-lock-add-keywords nil x))
+          '((("#?['`]*(\\|)"       . 'clojure-parens))
+            (("#?\\^?{\\|}"        . 'clojure-brackets))
+            (("\\[\\|\\]"          . 'clojure-braces))
+            ((":\\w+"              . 'clojure-keyword))
+            (("#?\""               0 'clojure-double-quote prepend))
+            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
+            (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)" 1 'clojure-java-call)))))
+
+(add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
